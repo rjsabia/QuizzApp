447da6e20c5912f9d8b05674fdb9ea4bc4ee2aad
Russell Sabia <rjsabia@gmail.com>
2016-12-08 11:53:20 -0800

    questionscheck answer completed, game functions, needs output and styling

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
-// $(document).ready(function(){
-  
-// });
- // Below is the array holding the objects with all test questions, and correct answers for Space awesomeness!!!
-(function() {
+
+ // array of objects holding questions, choices, and the correct answer
   var questions = [{
     question: "Earth's Moon is how many miles away?",
     choices: ['1 million miles', '500 miles', '2,380 miles', '238,900 miles'],
@@ -24,73 +21,60 @@
     choices: ['25,000 years', '10 years', '25 years', '1,000 years'],
     correctAnswer: 0,
   }];
-
-
-  // Below are global scope variables
+// Below are global scope variables
   //current question
   var questionCounter = 0;  
-  //Array with user choices
-  var selections = []; 
   //Quiz div object
   var quiz = $('#quiz'); 
   
+  var numCorrect = 0;
+
+  var totalQuestions = 5;
+
   // function call to display first question
   displayNext();
   
   //handler for the 'next' button
-  $('#next').on('click', function (e) {
-  	
-  	e.preventDefault();
+  $('#quiz').submit(function(event){
+    
+    event.preventDefault();
+
+    var userAnswer = $("input[name=answer]:checked").val();
+    
+    console.log(userAnswer);
     
     // Suspend click listener during fade animation
     if(quiz.is(':animated')) {        
     	
     	return false;
     }
-    // calls function choose for user input
-    choose();
-    
     // Checks if user selected a question, alerts user to pick a question if none selected
-    if (isNaN(selections[questionCounter])) {
+    if (userAnswer == undefined) {
     	
     	alert('Please make a selection, come on you want to learn dont you!');
     } 
 
     else {
-      	// increments counter to next question
+      	
+      	if (userAnswer == questions[questionCounter].correctAnswer){
+      		 
+      		 $('#output').html('Correct!');
+
+      		 numCorrect++;
+
+      	}
+      	else{
+      		$('#output').html('Incorrect!');
+      	}
       	questionCounter++;
       	// Calls up next question
       	displayNext();
     }
   });
   
-  // Click handler for the 'prev' button
-  $('#prev').on('click', function (e) {
-  	// prevents input submition to browser
-  	e.preventDefault();
-    
-    if(quiz.is(':animated')) {
-    	
-    	return false;
-    }
-    // calls function for user input
-    choose();
-
-    questionCounter--;
-
-    displayNext();
-  });
-  
   // Click handler for the 'Start Over' button
   $('#start').on('click', function (e) {
     
-    // prevents input submition to browser
-  	e.preventDefault();
-    
-    if(quiz.is(':animated')) {
-    	
-    	return false;
-    }
     // calls function to restart game
 	newGame();
 
@@ -139,13 +123,6 @@
     
     return radioList;
   }
-  // ******************************************************************
-  // Read in the user selection, and push value into an array when button click
-  function choose() {
-  	
-  	selections[questionCounter] = +$('input[name="answer"]:checked').val();
-  
-  }
   // *******************************************************************
   // Displays next requested question when called, and controls display of buttons (next, prev)
   function displayNext() {
@@ -158,20 +135,12 @@
       	// variable calls function for question creation, based on index value of the question counter
       	var nextQuestion = createQuestionElement(questionCounter);
         // fades in the next question
-        quiz.append(nextQuestion).fadeIn();
+        quiz.prepend(nextQuestion).fadeIn();
 
-        // Function below is greyed out at bottom of program, need mentor advise
-        // questionOutput();
-        
-        // Need to ask mentor what the value is of this, found it online but not 100% on use
-        if (!(isNaN(selections[questionCounter]))) {
-        	
-        	$('input[value='+selections[questionCounter]+']').prop('checked', true);
-        }
          // Controls display of 'prev' button
         if(questionCounter === 1){
         	
-        	$('#prev').show();
+        	$('#prev').show(); 
         } 
         else if(questionCounter === 0){
           
@@ -188,8 +157,6 @@
         	
         	$('#next').hide();
         	
-        	$('#prev').hide();
-        	
         	$('#start').show();
       }
     });
@@ -200,18 +167,8 @@
   	
   	var score = $('<p>',{id: 'question'});
     
-    var numCorrect = 0;
-    
-    for (var i = 0; i < selections.length; i++) {
-    	
-    	if (selections[i] === questions[i].correctAnswer) {
-        	
-        	numCorrect++;
-      }
-    }
-    
     score.append('You got ' + numCorrect + ' questions out of ' +
-                 questions.length + ' right!!!');
+                 totalQuestions + ' right!!!');
     return score;
   }
   // ************************************************************
@@ -226,26 +183,5 @@
 	 
 	 $('#start').hide();
   }
- // **********************************************
- // Need help with this, have to ask mentor, trying to output the correct answer if wrong
- // function questionOutput(){
-
- // 	$('#output').empty(); 
-
- // 	if (selections[questionCounter - 1] === questions[questionCounter - 1].correctAnswer){
-
- // 		$('#output').append('You got the last question right!');
- // 	}
- // 	else{
- 		
- // 		$('#output').append('Your last question was incorrect, the correct answer is ' +
- // 				questions.choices[questionCounter.correctAnswer];
- // 	}
- 	
- 	
- // }
- // **********************************************
-
- })();
  // **********************************************
  // End Of Program : )
